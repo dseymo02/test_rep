d2f7fa3fd28fb01cf1694001671d873008cac138
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-27 15:58:52 +0100

    got it to work

--- question4.py
+++ question4.py
@@ -8,7 +8,6 @@ def readAnswers(answers,size):
 	str = input()
 	answers = str.split()
 	studentnumber = answers.pop(0)
-	assert len(answers) == size
 	return studentnumber, answers
 
 def main():
@@ -23,9 +22,10 @@ def main():
 	while not finished:
 		studentnumber, answers = readAnswers(answers,size)
 		print(studentnumber, answers)
-		if studentnumber == 999:
+		if studentnumber == "999":
 			finished = True
 			print(studentnumber)
+	print(finished)
 	
 if __name__ == "__main__":
 	main()
