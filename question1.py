e0ff88026f9c04993963d64346aa8cdcdebb07e0
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-29 08:25:17 +0100

    more examples

--- question1.py
+++ question1.py
@@ -2,11 +2,22 @@ from random import randint
 
 # question 1a
 def fib(n):
-	''' function to print out fib numbers'''
+	''' function to print out fib numbers recurvisely'''
 	if n in [1,0]:
 		return 1
 	else:
 		return fib(n-1) + fib(n-2)
+
+def fibit(n):
+	prev = 0
+	curr = 1
+	print(prev,curr,' ',end='')
+	for i in range(2,n):
+		next = prev + curr
+		print(next,'',end='')
+		prev = curr
+		curr = next
+		
 
 def fibseq(n):
 	seq = []
@@ -37,6 +48,9 @@ def main():
 	# question 1b programme
 	l = 2000
 	print([i for i in fibseqlimit(l)])
+
+	n = 20
+	fibit(n)
 	
 	
 if __name__ == "__main__":
